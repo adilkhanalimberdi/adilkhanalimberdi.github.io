c6d2ea57d607c0016d00140a85eedf5a8eb84821
adilkhanalimberdi <119858127+adilkhanalimberdi@users.noreply.github.com>
2023-05-03 16:34:48 +0600

    Add files via upload

---
+++ keyboard/script.js
@@ -0,0 +1,342 @@
+var res = document.getElementById("res");
+var check = 0;
+
+res = "";
+
+function a() {
+    if (check%2 == 1){
+        res += "A";
+    } else {
+        res += "a";
+    }
+    document.getElementById("res").innerHTML = res;
+    //console.log(res);
+}
+
+function b() {
+    if (check%2 == 1){
+        res += "B";
+    } else {
+        res += "b";
+    }
+    document.getElementById("res").innerHTML = res;
+    //console.log(res);
+}
+
+function c() {
+    if (check%2 == 1){
+        res += "C";
+    } else {
+        res += "c";
+    }
+    document.getElementById("res").innerHTML = res;
+    //console.log(res);
+}
+
+function d() {
+    if (check%2 == 1){
+        res += "D";
+    } else {
+        res += "d";
+    }
+    document.getElementById("res").innerHTML = res;
+    //console.log(res);
+}
+
+function e() {
+    if (check%2 == 1){
+        res += "E";
+    } else {
+        res += "e";
+    }
+    document.getElementById("res").innerHTML = res;
+    //console.log(res);
+}
+
+function f() {
+    if (check%2 == 1){
+        res += "F";
+    } else {
+        res += "f";
+    }
+    document.getElementById("res").innerHTML = res;
+    //console.log(res);
+}
+
+function g() {
+    if (check%2 == 1){
+        res += "G";
+    } else {
+        res += "g";
+    }
+    document.getElementById("res").innerHTML = res;
+    //console.log(res);
+}
+
+function h() {
+    if (check%2 == 1){
+        res += "H";
+    } else {
+        res += "h";
+    }
+    document.getElementById("res").innerHTML = res;
+    //console.log(res);
+}
+
+function i() {
+    if (check%2 == 1){
+        res += "I";
+    } else {
+        res += "i";
+    }
+    document.getElementById("res").innerHTML = res;
+    //console.log(res);
+}
+
+function j() {
+    if (check%2 == 1){
+        res += "J";
+    } else {
+        res += "j";
+    }
+    document.getElementById("res").innerHTML = res;
+    //console.log(res);
+}
+
+function k() {
+    if (check%2 == 1){
+        res += "K";
+    } else {
+        res += "k";
+    }
+    document.getElementById("res").innerHTML = res;
+    //console.log(res);
+}
+
+function l() {
+    if (check%2 == 1){
+        res += "L";
+    } else {
+        res += "l";
+    }
+    document.getElementById("res").innerHTML = res;
+    //console.log(res);
+}
+
+function m() {
+    if (check%2 == 1){
+        res += "M";
+    } else {
+        res += "m";
+    }
+    document.getElementById("res").innerHTML = res;
+    //console.log(res);
+}
+
+function n() {
+    if (check%2 == 1){
+        res += "N";
+    } else {
+        res += "n";
+    }
+    document.getElementById("res").innerHTML = res;
+    //console.log(res);
+}
+
+function o() {
+    if (check%2 == 1){
+        res += "O";
+    } else {
+        res += "o";
+    }
+    document.getElementById("res").innerHTML = res;
+    //console.log(res);
+}
+
+function p() {
+    if (check%2 == 1){
+        res += "P";
+    } else {
+        res += "p";
+    }
+    document.getElementById("res").innerHTML = res;
+    //console.log(res);
+}
+
+function q() {
+    if (check%2 == 1){
+        res += "Q";
+    } else {
+        res += "q";
+    }
+    document.getElementById("res").innerHTML = res;
+    //console.log(res);
+}
+
+function r() {
+    if (check%2 == 1){
+        res += "R";
+    } else {
+        res += "r";
+    }
+    document.getElementById("res").innerHTML = res;
+    //console.log(res);
+}
+
+function s() {
+    if (check%2 == 1){
+        res += "S";
+    } else {
+        res += "s";
+    }
+    document.getElementById("res").innerHTML = res;
+    //console.log(res);
+}
+
+function t() {
+    if (check%2 == 1){
+        res += "T";
+    } else {
+        res += "t";
+    }
+    document.getElementById("res").innerHTML = res;
+    //console.log(res);
+}
+
+function u() {
+    if (check%2 == 1){
+        res += "U";
+    } else {
+        res += "u";
+    }
+    document.getElementById("res").innerHTML = res;
+    //console.log(res);
+}
+
+function v() {
+    if (check%2 == 1){
+        res += "V";
+    } else {
+        res += "v";
+    }
+    document.getElementById("res").innerHTML = res;
+    //console.log(res);
+}
+
+function w() {
+    if (check%2 == 1){
+        res += "W";
+    } else {
+        res += "w";
+    }
+    document.getElementById("res").innerHTML = res;
+    //console.log(res);
+}
+
+function x() {
+    if (check%2 == 1){
+        res += "X";
+    } else {
+        res += "x";
+    }
+    document.getElementById("res").innerHTML = res;
+    //console.log(res);
+}
+
+function y() {
+    if (check%2 == 1){
+        res += "Y";
+    } else {
+        res += "y";
+    }
+    document.getElementById("res").innerHTML = res;
+    //console.log(res);
+}
+
+function z() {
+    if (check%2 == 1){
+        res += "Z";
+    } else {
+        res += "z";
+    }
+    document.getElementById("res").innerHTML = res;
+    //console.log(res);
+}
+
+function space() {
+    res += " ";
+    document.getElementById("res").innerHTML = res;
+    console.log(res);
+}
+
+function shift() {
+    check+=1;
+    if (check%2 == 1) {
+        document.getElementById("a").innerHTML = "A";
+        document.getElementById("b").innerHTML = "B";
+        document.getElementById("c").innerHTML = "C";
+        document.getElementById("d").innerHTML = "D";
+        document.getElementById("e").innerHTML = "E";
+        document.getElementById("f").innerHTML = "F";
+        document.getElementById("g").innerHTML = "G";
+        document.getElementById("h").innerHTML = "H";
+        document.getElementById("i").innerHTML = "I";
+        document.getElementById("j").innerHTML = "J";
+        document.getElementById("k").innerHTML = "K";
+        document.getElementById("l").innerHTML = "L";
+        document.getElementById("m").innerHTML = "M";
+        document.getElementById("n").innerHTML = "N";
+        document.getElementById("o").innerHTML = "O";
+        document.getElementById("p").innerHTML = "P";
+        document.getElementById("q").innerHTML = "Q";
+        document.getElementById("r").innerHTML = "R";
+        document.getElementById("s").innerHTML = "S";
+        document.getElementById("t").innerHTML = "T";
+        document.getElementById("u").innerHTML = "U";
+        document.getElementById("v").innerHTML = "V";
+        document.getElementById("w").innerHTML = "W";
+        document.getElementById("x").innerHTML = "X";
+        document.getElementById("y").innerHTML = "Y";
+        document.getElementById("z").innerHTML = "Z";
+    } else {
+        document.getElementById("a").innerHTML = "a";
+        document.getElementById("b").innerHTML = "b";
+        document.getElementById("c").innerHTML = "c";
+        document.getElementById("d").innerHTML = "d";
+        document.getElementById("e").innerHTML = "e";
+        document.getElementById("f").innerHTML = "f";
+        document.getElementById("g").innerHTML = "g";
+        document.getElementById("h").innerHTML = "h";
+        document.getElementById("i").innerHTML = "i";
+        document.getElementById("j").innerHTML = "j";
+        document.getElementById("k").innerHTML = "k";
+        document.getElementById("l").innerHTML = "l";
+        document.getElementById("m").innerHTML = "m";
+        document.getElementById("n").innerHTML = "n";
+        document.getElementById("o").innerHTML = "o";
+        document.getElementById("p").innerHTML = "p";
+        document.getElementById("q").innerHTML = "q";
+        document.getElementById("r").innerHTML = "r";
+        document.getElementById("s").innerHTML = "s";
+        document.getElementById("t").innerHTML = "t";
+        document.getElementById("u").innerHTML = "u";
+        document.getElementById("v").innerHTML = "v";
+        document.getElementById("w").innerHTML = "w";
+        document.getElementById("x").innerHTML = "x";
+        document.getElementById("y").innerHTML = "y";
+        document.getElementById("z").innerHTML = "z";
+    }
+}
+
+function back() {
+    if (res == "") return 0;
+    let size = res.length - 1;
+    let temp = 0;
+    let s = "";
+    while (temp != size) {
+        s += res[temp];
+        temp++;
+    }
+    res = s;
+    document.getElementById("res").innerHTML = res;
+}
