81509eb8bd9dfe32883d1dc70a22bb9de54b210d
adilkhanalimberdi <adilkhankerimshe@gmail.com>
2026-08-18 23:08:13 +0500

    fix: add quotes to schema in entities

--- backend/src/main/java/com/alimberdi/backend/auth/model/entity/User.java
+++ backend/src/main/java/com/alimberdi/backend/auth/model/entity/User.java
@@ -10,7 +10,7 @@ import java.time.Instant;
 import java.util.UUID;
 
 @Entity
-@Table(name = "users", schema = "authorization")
+@Table(name = "users", schema = "`authorization`")
 @Getter
 @Setter
 @ToString
